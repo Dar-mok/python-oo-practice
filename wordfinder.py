22932cea79e9e06d1a1db35da157cdf28ae37cef
Russell Jones <justyrones@gmail.com>
2023-04-18 18:22:31 -0600

    done

--- wordfinder.py
+++ wordfinder.py
@@ -2,21 +2,23 @@ from random import choice
 
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
-    def __init__(self, path):
+    def __init__(self, file):
         """Creates an instance of the WordFinder class"""
-        #self.path = path
-        self.words = list(open(f"{path}", "r"))
+        self.words = self.parse(open(file))
+
 
     def random(self):
         """Return a random word from a list of words"""
-        word = choice(self.words)
-        return word[:-1:]
+        return choice(self.words)
+
+    def parse(self, file):
+        """Removes whitespace around each line  """
+        return [line.strip() for line in file]
+
 
 class SpecialWordFinder(WordFinder):
-    def __init__(self, path):
-        """Creates an instance of the SpecialWordFinder class"""
-        super().__init__(path)
 
-    def random(self):
-        clean_words = [word[:-1:] for word in self.words if not word.startswith('\\') and not word.startswith('#')]
-        return choice(clean_words)
+    def parse(self, file):
+        """Goes through a list of words and removes any words/lines starting with a # symbol and removes line breaks"""
+
+        return  [word for word in super().parse(file) if word and not word.startswith('#')]
